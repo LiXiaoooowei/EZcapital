938057c2ad38c2ca0d6ebee1764210d497f74828
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-06-13 23:30:08 +0800

    add category icon

--- Pay.js
+++ Pay.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, {Component} from "react";
+import {View} from 'react-native';
+import {Container, Body, Header, Left, Right, Icon, Title, Button,Content, Text} from "native-base";
 
-import {Container, Body, Header, Left, Right, Icon, Title, Button,} from "native-base";
-
-export default class Pay extends React.Component {
-
+export default class Pay extends Component {
     render() {
         return (
             <Container>
@@ -21,7 +20,29 @@ export default class Pay extends React.Component {
                     </Body>
                     <Right />
                 </Header>
+                <Content>
+                    <View style = {{flex: 1, flexDirection: 'row', marginTop: 10, marginLeft: 5}}>
+                        <Button primary style = {{"width": BUTTON_WIDTH, "height": BUTTON_WIDTH, "borderRadius": BUTTON_RADIUS}}>
+                            <Icon name = "pizza" size={ICON_SIZE}/>
+                        </Button>
+                        <Button success style = {{"width": BUTTON_WIDTH, "height": BUTTON_WIDTH, "borderRadius": BUTTON_RADIUS}}>
+                            <Icon name = "bicycle" size={ICON_SIZE}/>
+                        </Button>
+                        <Button info style = {{"width": BUTTON_WIDTH, "height": BUTTON_WIDTH, "borderRadius": BUTTON_RADIUS}}>
+                            <Icon name = "medkit" size={ICON_SIZE}/>
+                        </Button>
+                        <Button warning style = {{"width": BUTTON_WIDTH, "height": BUTTON_WIDTH, "borderRadius": BUTTON_RADIUS}}>
+                            <Icon name = "paper-plane" size={ICON_SIZE} />
+                        </Button>
+                        <Button danger style = {{"width": BUTTON_WIDTH, "height": BUTTON_WIDTH, "borderRadius": BUTTON_RADIUS}}>
+                            <Icon name = "person" size={ICON_SIZE}/>
+                        </Button>
+                    </View>
+                </Content>
             </Container>
         );
     }
 }
+const BUTTON_WIDTH = 70;
+const BUTTON_RADIUS = BUTTON_WIDTH/2.0;
+const ICON_SIZE = 50;
