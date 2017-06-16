3accb73c574c9ced036fce25d58c8363dd3bdde9
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-06-16 12:22:28 +0800

    wire up tab bar scroll view for personal finance page

---
+++ PF_Products.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {View, Image, TouchableHighlight} from 'react-native';
+import {Container, Body, Header, Left, Right, Icon, Title, Button,Content, Text, List, ListItem, Thumbnail, Footer} from "native-base";
+
+
+export default class PF_Products extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Products</Text>
+            </View>
+        );
+    }
+}
--- PersonalFinance.js
+++ PersonalFinance.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react";
-import {View, Image, TouchableHighlight} from 'react-native';
-import {Container, Body, Header, Left, Right, Icon, Title, Button,Content, Text, List, ListItem, Thumbnail, Card, CardItem, Fab} from "native-base";
-
-
+import {View, Image, ScrollView} from 'react-native';
+import {Container, Body, Header, Left, Right, Icon, Title, Button,Content, Text, List, ListItem, Thumbnail, Footer} from "native-base";
+import ScrollableTabView, {ScrollableTabBar} from 'react-native-scrollable-tab-view';
+import Today from './PF_Today';
+import Products from './PF_Products';
+import Account from './PF_MyAccount';
 export default class PersonalFinance extends Component {
 
     render() {
@@ -22,9 +24,11 @@ export default class PersonalFinance extends Component {
                     </Body>
                     <Right />
                 </Header>
-                <Content>
-
-                </Content>
+                    <ScrollableTabView style = {{backgroundColor: 'white', borderTopColor: 'lightgray'}} tabBarPosition = "bottom" renderTabBar={()=><ScrollableTabBar/>}>
+                        <Today tabLabel="Today" />
+                        <Products tabLabel="Products" />
+                        <Account tabLabel="My Account" />
+                    </ScrollableTabView>
             </Container>
         );
     }
